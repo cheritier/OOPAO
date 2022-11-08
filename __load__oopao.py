d4dc2fbb4bc3806229343b42955863dc43689be3
cheritier <cheritie@HQL025838.ads.eso.org>
2022-11-08 15:24:22 +0100

    update setup files

--- __load__oopao.py
+++ __load__oopao.py
@@ -28,25 +28,27 @@ def load_oopao():
     print('======================================================================================>      ')
     print('\n')
 
-    n_max = 20
+    n_max = 40
     if path ==[]:
         while path == [] and counter!= n_max :
             # print('Looking for AO_Modules in the parent repositories...')
             name_repository = '../'+name_repository
             path = glob.glob(name_repository,recursive = True)
-            
-            print(counter, ' ', end='\r', flush=True)
-
             counter += 1 
         nameFolder =path[0] 
         sys.path.append(nameFolder[:-10])
     if counter == n_max:
         raise RuntimeError('The AO modules repository could not be found. Make sure that your script is located in a sub-folder of the OOPAO repository!')
     # check the version of numpy libraries
-    config = np.__config__.blas_mkl_info['libraries'][0]
-    if config != 'mkl_rt':
+    try:
+        config = np.__config__.blas_mkl_info['libraries'][0]
+        if config != 'mkl_rt':
+            print('**************************************************************************************************************************************************************')
+            print('NUMPY WARNING: OOPAO multi-threading requires to use numpy built with mkl library! If you are using AMD or Apple processors the code could be single threaded!')
+            print('**************************************************************************************************************************************************************')
+    except:
         print('**************************************************************************************************************************************************************')
-        print('NUMPY WARNING: OOPAO multi-threading requires to use numpy built with mkl library! If you are using AMD or Apple processors the code could be single threaded!')
+        print('NUMPY WARNING: mkl blas not found! Multi-threading may not work as expected.')
         print('**************************************************************************************************************************************************************')
     # print('OOPAO has been properly initialized!')
     # print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
